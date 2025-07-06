221ff9f6c2a1aa7fbce2d20a2265c46da3624edb
AbdullahMufti <abdullahmufty@gmail.com>
2025-07-07 01:22:52 +0500

    added og images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Noto_Naskh_Arabic } from "next/font/google";
 import "./globals.css";
+import { ImageResponse } from "next/og";
 
 const notoNaskhArabic = Noto_Naskh_Arabic({
   variable: "--font-noto-naskh-arabic",
@@ -12,6 +13,21 @@ export const metadata: Metadata = {
   description: "قصص كاميل كيلاني",
   icons: {
     icon: "/kk.jpg",
+  },
+  openGraph: {
+    images: ["/kk.jpg"],
+    title: "قصص كاميل كيلاني",
+    description: "قصص كاميل كيلاني",
+    url: "https://kamilkilani.com",
+    siteName: "قصص كاميل كيلاني",
+    locale: "ar-SA",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "قصص كاميل كيلاني",
+    description: "قصص كاميل كيلاني",
+    images: ["/kk.jpg"],
   },
 };
 
